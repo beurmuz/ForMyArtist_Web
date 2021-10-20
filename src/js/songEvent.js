44264ea9f881341aa85ad6cc0a4eb7fd2062841e
beurmuz <fallinta2@gmail.com>
2021-10-21 00:57:46 +0900

    211021 > songs페이지 json 연동

--- src/js/songEvent.js
+++ src/js/songEvent.js
@@ -1,25 +1,27 @@
 // Fetch the items from the JSON file
-// function loadItems() {
-//     return fetch('data/data.json')
-//       .then(response => response.json())
-//       .then(json => json.items);
-//   }
+function loadItems() {
+    return fetch('../../data/data.json')
+      .then(response => response.json())
+      .then(json => json.items);
+  }
   
-//   // Update the list with the given items
-//   function displayItems(items) {
-//     const container = document.querySelector('.items');
-//     container.innerHTML = items.map(item => createHTMLString(item)).join('');
-//   }
+  // Update the list with the given items
+  function displayItems(items) {
+    const container = document.querySelector('.items');
+    console.log(container);
+    console.log(items.map(values => createHTMLString(values)).join(''));
+    container.innerHTML = items.map(values => createHTMLString(values)).join('');
+  }
   
-//   // Create HTML list item from the given data item
-//   function createHTMLString(item) {
-//     return `
-//       <li class="item">
-//           <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
-//           <span class="item__description">${item.gender}, ${item.size}</span>
-//       </li>
-//       `;
-//   }
+  // Create HTML list item from the given data item
+  function createHTMLString(values) {
+    return `
+      <li class="item">
+        <i class="fas fa-compact-disc"></i> <div alt="${values.type}"></div>
+        <span class="item__description">${values.gender}, ${values.size}</span>
+      </li>
+      `;
+  }
   
 //   function onButtonClick(event, items) {
 //     const dataset = event.target.dataset;
@@ -40,14 +42,16 @@
 //     buttons.addEventListener('click', event => onButtonClick(event, items));
 //   }
   
-//   // main
-//   loadItems()
-//     .then(items => {
-//       displayItems(items);
-//       setEventListeners(items);
-//     })
-//     .catch(console.log);
+  // main
+loadItems()
+    .then(items => {
+      displayItems(items);
+      setEventListeners(items);
+})
+.catch(console.log);
   
+
+
 
 let songsList = ["iffy", "party", "깡", "달링", "RING RING", "RESPECT", "GIDDY UP", "랑데뷰", "The Purge", "Is It Love", "Me&Bae", "WHY YOU?", "그래 그냥 내게 바로", "How We Rock", "JUST U", "침대 노래", "FIRE", "너의 밤", "ㄱ에서부터 0에서부터", "World Domination"]
 // User가 입력한 input태그의 값 가져오기
