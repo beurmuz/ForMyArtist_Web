ff1244be021e4267cb07760632fc722f7e0f5b03
beurmuz <fallinta2@gmail.com>
2021-11-29 16:29:27 +0900

    DEV > introduce > 수정 전

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,17 +1,3 @@
-const mainTag = document.querySelector('main');
-const introBox = document.querySelector('.introBox');
-
-function darkModeHandler(self) {
-    if(self.value === 'Night') {
-        mainTag.style.backgroundColor = 'black';
-        introBox.style.color = 'white';
-        self.value = 'daytime';
-    } else {
-        mainTag.style.backgroundColor = 'white';
-        mainTag.style.color = 'black';
-        self.value = 'Night';
-    } 
-} 
 
 const abImgBox = document.querySelector('.abImgBox');
 const songBox = document.querySelector('.songsBox');
