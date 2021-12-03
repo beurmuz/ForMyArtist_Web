872e8b70ed5887312af58b1f24f02c7682e1bc69
beurmuz <fallinta2@gmail.com>
2021-12-03 15:31:15 +0900

    DEV #5 intro페이지 완성

---
+++ src/js/intro.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let section = document.getElementsByTagName('section');
+let dotBtn = document.querySelectorAll('.pointWrap li');
+
+let pageNum = 0;
+let totalNum = section.length;
+
+
+for(let i = 0; i < dotBtn.length; i++) {
+    dotBtnOnClick(i);
+    // dotBtn[i].onclick = function() {
+    //     console.log(i);
+    // }
+}
+
+function dotBtnOnClick(index) {
+    dotBtn[index].onclick = function() {
+        // console.log(index);
+        pageNum = index;
+        pageOnChange();
+
+        window.scrollTo({
+            top: section[pageNum].offsetTop,
+            behavior:  'smooth'        
+        });
+    }
+}
+
+window.addEventListener('scroll', (event) => {
+    let scroll = this.scrollY;
+
+    for(let i = 0; i < totalNum; i++) {
+        if(scroll > section[i].offsetTop - window.outerHeight/1.5 && scroll < section[i].offsetTop - window.outerHeight/1.5 + section[i].offsetHeight) {
+            pageNum = i;
+            break;
+        }
+        // 페이지 이동
+        pageOnChange();
+    }
+});
+
+function pageOnChange() {
+    for(let i = 0; i < totalNum; i++) {
+        section[i].classList.remove('active');
+        dotBtn[i].classList.remove('active');
+    }
+    section[pageNum].classList.add('active');
+    dotBtn[pageNum].classList.add('active');
+}
+
+pageOnChange();
